281e77aefafaec11e51e4c617f38079f2d0455e7
Ivan5646 <Mike000027@yandex.ru>
2017-04-15 21:28:21 +0700

    first

--- home.php
+++ home.php
@@ -1,26 +1,79 @@
+<?php
+ ob_start();
+ session_start();
+ require_once 'dbconnect.php';
+ 
+ // if session is not set this will redirect to login page
+ if( !isset($_SESSION['user']) ) {
+  header("Location: index.php");
+  exit;
+ }
+ // select loggedin users detail
+ $res=mysql_query("SELECT * FROM users WHERE userId=".$_SESSION['user']);
+ $userRow=mysql_fetch_array($res);
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="common.js"></script>
-  <style>
-    header ul li, footer ul li {float: left; list-style-type: none; padding-right: 10px;}
-    header{margin:0 0 30px 0; background-color: grey; color: white; height: 30px;}
-    footer{background-color: #C8D8DE; height: 30px;}
-    .current{color: yellow;}
-  </style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome - <?php echo $userRow['userEmail']; ?></title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
-<?php $page = "home"; ?>
-<?php include('header.php'); ?>
-<h3><?php echo "hey hey echoed by php"; ?></h3>
-<h2>This is Home page</h2>
-<section>text <br>text<br>text<br>text<br>text<br>text<br>text<br>text<br>text<br>text</section>
-<?php include('footer.php'); ?>
+
+ <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://www.codingcage.com">Coding Cage</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://www.codingcage.com/2015/01/user-registration-and-login-script-using-php-mysql.html">Back to Article</a></li>
+            <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
+            <li><a href="http://www.codingcage.com/search/label/PHP">PHP</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+     <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['userEmail']; ?>&nbsp;<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav> 
+
+ <div id="wrapper">
+
+ <div class="container">
+    
+     <div class="page-header">
+     <h3>Coding Cage - Programming Blog</h3>
+     </div>
+        
+        <div class="row">
+        <div class="col-lg-12">
+        <h1>Focuses on PHP, MySQL, Ajax, jQuery, Web Design and more...</h1>
+        </div>
+        </div>
+    
+    </div>
+    
+    </div>
+    
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
 </body>
 </html>
+<?php ob_end_flush(); ?>
